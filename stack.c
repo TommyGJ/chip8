51b5f54bafcdaa2d612d2e4833c2cc79867b7e47
Tommy Johnson <tommy.johnson@yale.edu>
2018-06-02 16:09:24 -0700

    added stack

---
+++ stack.c
@@ -0,0 +1,47 @@
+#include "chip.h"
+void updateLocations(struct linkedList *, int);
+struct linkedList *push(struct linkedList *list, unsigned short data){
+	struct linkedList *newEntry;
+	if((newEntry = (struct linkedList *)malloc(sizeof(struct linkedList))) == NULL){
+		exit(0);
+	}
+	
+	newEntry -> next = list;
+	newEntry -> address = data;
+	
+	updateLocations(newEntry, 0);	
+	return newEntry;	
+}
+
+void updateLocations(struct linkedList *list, int entry){
+	if(entry == 16){
+		printf("out of bounds");
+		exit(0);
+	}
+	if(list != NULL){
+		list -> location = entry;
+		updateLocations(list -> next, entry + 1);
+	}
+}
+struct linkedList *pop(struct linkedList *list){
+	if(list == NULL){
+		return NULL;
+	}
+	struct linkedList *newHead = list -> next;
+	free(list);
+
+	updateLocations(newHead, 0);
+	return newHead;
+
+}/*
+int main(){
+	chip8 c8;
+	c8.stack = NULL;
+	c8.stack = push(c8.stack, 0x210);
+	c8.stack = push(c8.stack, 0x300);
+	c8.stack = push(c8.stack, 0x500);
+
+	printf("%x at %i and %x at %i\n", c8.stack -> address,c8.stack -> location, c8.stack -> next -> address, c8.stack -> next -> location);
+	c8.stack = pop(c8.stack);
+	printf("%x at %i and %x at %i\n", c8.stack -> address,c8.stack -> location, c8.stack -> next -> address, c8.stack -> next -> location);
+}*/
---
+++ stack.h
@@ -0,0 +1,8 @@
+#include "chip.h"
+
+void updateLocations(struct linkedList *, int);
+
+struct linkedList *push(struct linkedList *, unsigned short);
+
+struct linkedList *pop(struct linkedList *);
+
